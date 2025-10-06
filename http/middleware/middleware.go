6ac8b08ecc3c5fcc92c9deb0c1ecc1bb18e3b7a9
AJ ONeal <aj@therootcompany.com>
2025-10-06 01:50:03 -0600

    chore(http/middleware): correct CC0-1.0 SPDX license header

--- http/middleware/middleware.go
+++ http/middleware/middleware.go
@@ -1,10 +1,13 @@
-// Copyright 2025 AJ ONeal <aj@therootcompany.com> (https://therootcompany.com)
+// Authored in 2025 by AJ ONeal <aj@therootcompany.com> (https://therootcompany.com)
 //
-// This Source Code Form is subject to the terms of the Mozilla Public
-// License, v. 2.0. If a copy of the MPL was not distributed with this
-// file, You can obtain one at https://mozilla.org/MPL/2.0/.
+// To the extent possible under law, the author(s) have dedicated all copyright
+// and related and neighboring rights to this software to the public domain
+// worldwide. This software is distributed without any warranty.
 //
-// SPDX-License-Identifier: MPL-2.0
+// You should have received a copy of the CC0 Public Domain Dedication along with
+// this software. If not, see <https://creativecommons.org/publicdomain/zero/1.0/>.
+//
+// SPDX-License-Identifier: CC0-1.0
 
 package middleware
 
